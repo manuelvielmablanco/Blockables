4bff1e96f837e88e06223dfa4cd0da30d2e5bf59
manuelvielmablanco <manuel.vielma@e-pisteme.tech>
2026-05-18 19:05:20 +0200

    Expand io_pinmode PIN dropdown to include analog pins A0-A5

--- src/blockly/blocks/io.ts
+++ src/blockly/blocks/io.ts
@@ -3,18 +3,21 @@ import * as Blockly from 'blockly';
 const DIGITAL_PINS: [string, string][] = [['2','2'],['3','3'],['4','4'],['5','5'],['6','6'],['7','7'],['8','8'],['9','9'],['10','10'],['11','11'],['12','12'],['13','13']];
 const ANALOG_PINS: [string, string][] = [['A0','A0'],['A1','A1'],['A2','A2'],['A3','A3'],['A4','A4'],['A5','A5']];
 const PWM_PINS: [string, string][] = [['3','3'],['5','5'],['6','6'],['9','9'],['10','10'],['11','11']];
+// pinMode acepta cualquier pin GPIO: en Uno/Nano los A0-A5 también se pueden
+// usar como digitales (mapean a D14-D19).
+const PINMODE_PINS: [string, string][] = [...DIGITAL_PINS, ...ANALOG_PINS];
 
 Blockly.Blocks['io_pinmode'] = {
   init: function (this: Blockly.Block) {
     this.appendDummyInput()
       .appendField('configurar PIN')
-      .appendField(new Blockly.FieldDropdown(DIGITAL_PINS) as Blockly.Field, 'PIN')
+      .appendField(new Blockly.FieldDropdown(PINMODE_PINS) as Blockly.Field, 'PIN')
       .appendField('como')
       .appendField(new Blockly.FieldDropdown([['ENTRADA', 'INPUT'], ['SALIDA', 'OUTPUT'], ['ENTRADA PULLUP', 'INPUT_PULLUP']]) as Blockly.Field, 'MODE');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setStyle('io_blocks');
-    this.setTooltip('Configura un pin como entrada o salida');
+    this.setTooltip('Configura un pin como entrada o salida (acepta digitales 2-13 y analógicos A0-A5)');
   },
 };
 
